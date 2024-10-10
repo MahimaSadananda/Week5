4e69c772837a00230265b544f1c6e2aa66288d17
Mahima Sadananda <71080692+MahimaSadananda@users.noreply.github.com>
2024-10-10 19:54:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def predict():
     return render_template('index.html', prediction_text='Passenger - {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
